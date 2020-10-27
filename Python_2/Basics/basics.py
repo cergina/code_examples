5e21033d0288f1931f77eb16cfd51f13cfe2a7e4
Maros Cerget <mcerget@gmail.com>
2020-10-27 15:47:44 +0100

    update

--- Python_2/Basics/basics.py
+++ Python_2/Basics/basics.py
@@ -11,5 +11,9 @@ len(sys.argv)
 # vypis argumentu 1
 print("Here is the argument 1, {0}".format(sys.argv[1]))
 
+# vypis argumentu '10110111' v bin, dec, hex
+# {1:3} prvy argument, 3 miesta rezervuj, 2x rezervuj dva zobraz v hexa
+print("{0}, {1:3}, {1:2x}".format("10110111", int("10110111", 2), int("10110111", 2)))
+
 # ukazka vystupu do error file outputu na linuxe
 sys.stderr.write("Name of a the script {0}\n".format(sys.argv[0]))
